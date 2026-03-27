c3e9db08047cd52961c98f328dc056d7b0623863
Selone-x <hramtsov.vadik@yandex.ru>
2026-03-27 21:14:11 +0300

    #3: add sieve cpp/py

---
+++ sieve of Eratosthenes/sieve.py
@@ -0,0 +1,12 @@
+def sieve(n):
+    is_prime = [True] * (n + 1)
+    is_prime[0] = is_prime[1] = False
+
+    i = 2
+    while i * i <= n:
+        if is_prime[i]:
+            for j in range(i * i, n + 1, i):
+                is_prime[j] = False
+        i += 1
+
+    return [i for i in range(2, n + 1) if is_prime[i]]
