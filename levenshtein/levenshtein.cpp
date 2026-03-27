0f15f29d26c0f7efbbd699815f6133d545cc3861
Selone-x <152082173+Selone-x@users.noreply.github.com>
2026-03-27 21:30:50 +0300

    #5: add levenshtein (#6)

---
+++ levenshtein/levenshtein.cpp
@@ -0,0 +1,31 @@
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+int damerau_levenshtein(const std::string& s1, const std::string& s2) {
+    int len1 = s1.size(), len2 = s2.size();
+    std::vector<std::vector<int>> dp(len1 + 1, std::vector<int>(len2 + 1, 0));
+
+    for (int i = 0; i <= len1; ++i) dp[i][0] = i;
+    for (int j = 0; j <= len2; ++j) dp[0][j] = j;
+
+    for (int i = 1; i <= len1; ++i) {
+        for (int j = 1; j <= len2; ++j) {
+            int cost = (s1[i - 1] == s2[j - 1]) ? 0 : 1;
+
+            dp[i][j] = std::min({
+                dp[i - 1][j] + 1,       // удаление
+                dp[i][j - 1] + 1,       // вставка
+                dp[i - 1][j - 1] + cost // замена / совпадение
+            });
+
+            // транспозиция соседних символов
+            if (i > 1 && j > 1 && s1[i - 1] == s2[j - 2] && s1[i - 2] == s2[j - 1]) {
+                dp[i][j] = std::min(dp[i][j], dp[i - 2][j - 2] + cost);
+            }
+        }
+    }
+
+    return dp[len1][len2];
+}
